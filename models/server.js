0535a7456f649ae58ee8acda34dc2eeeae86407a
Ezequiel Suarez <ezegsuarez@gmail.com>
2024-09-18 12:52:19 -0300

    Config

--- models/server.js
+++ models/server.js
@@ -2,24 +2,24 @@ const express = require('express')
 
 class Server {
   constructor () {
-    this.app = express()
-    this.port = process.env.PORT || 3000
-    this.middleware()
-    this.rutas()
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.middleware();
+    this.rutas();
   }
 
   middleware () {
-    this.app.use(express.static('public'))
+    this.app.use(express.static('public'));
   }
 
   rutas () {
-    this.app.use('/api/v1/empleados', require('../routes/empleados'))
+    this.app.use('/api/v1/empleados', require('../routes/empleados'));
   }
 
   listen () {
     this.app.listen(this.port, () => {
       console.log(`La API esta escuchando en el this.PORT ${this.port}`)
-    })
+    });
   }
 }
 
