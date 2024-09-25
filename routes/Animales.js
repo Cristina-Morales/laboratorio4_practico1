23503fc37e58a90716c70bf4409065d517f66177
Cristina Morales <crisvikim@hotmail.com>
2024-09-25 17:46:40 -0300

    corrigo errores que detecto Standard

--- routes/Animales.js
+++ routes/Animales.js
@@ -1,9 +1,9 @@
 const { Router } = require('express')
-const { get, getAnimales, getAnimalByID} = require('../controllers/Animales')
+const { getAnimales, getAnimalByID } = require('../controllers/Animales')
 
 const rutas = Router()
 
-//obtener el listado de animales y buscar
+// obtener el listado de animales y buscar
 rutas.get('/animales', getAnimales)
 // obtener animal en particular
 rutas.get('/animales/:idAnimal', getAnimalByID)
