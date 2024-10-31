acd695ce00151fb95305398b3a0d4f81327132da
Ezequiel Suarez <112830760+EzequielSuarez24@users.noreply.github.com>
2024-10-31 20:24:26 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const axios = require('axios');
+
+
 class Server {
   constructor () {
     this.app = express()
@@ -9,13 +12,32 @@ class Server {
 
   rutas () {
     this.app.use('/api/v1/', require('../routes/Gatos')) // Cristina Morales
-    // aca tambien va tu app
   }
 
   listen () {
     this.app.listen(this.port, () => {
       console.log('Cargo bien')
     })
+  }
+
+  // Método estático para obtener el token de Spotify Ezequiel Suarez
+  static async getSpotifyToken() {
+    try {
+      const response = await axios({
+        url: 'https://accounts.spotify.com/api/token',
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64'),
+        },
+        data: 'grant_type=client_credentials',
+      });
+
+      return response.data.access_token;
+    } catch (error) {
+      console.error('Error al obtener el token de Spotify:', error.message);
+      throw new Error('No se pudo obtener el token de Spotify');
+    }
   }
 }
 module.exports = Server
